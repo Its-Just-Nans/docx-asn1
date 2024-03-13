8c39f972d56f92a901ec1e33e772d6d77ae08d2c
n4n5 <its.just.n4n5@gmail.com>
2024-03-13 17:48:37 +0100

    refactor

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Decode the asn1 in docx
 
 ```sh
 # download the docx
-cd ~/Downloads
-python decode_asn1.py
+
+python decode_asn1.py file.docx output.asn1
+# or
+python decode_asn1.py file.docx > output.asn1
 ```
 
-Why `docx` for specificaiton @3gpp ?
+Question for @3gpp: why `docx` from for the specification ?
--- decode_asn1.py
+++ decode_asn1.py
@@ -1,9 +1,12 @@
-import docx
-import os
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from docx import Document
+from sys import argv
 
 
 def extract_text_from_docx(filename):
-    doc = docx.Document(filename)
+    doc = Document(filename)
     full_text = []
     inside_range = False
     for para in doc.paragraphs:
@@ -20,29 +23,17 @@ def extract_text_from_docx(filename):
     return "\n".join(full_text)
 
 
-def get_first_docx_filename(directory_path):
-    # Get a list of all files in the specified directory
-    all_files = os.listdir(directory_path)
-
-    # Filter the list to include only .docx files
-    docx_files = [
-        filename for filename in all_files if filename.lower().endswith(".docx")
-    ]
-
-    # Return the first .docx file name (if any)
-    if docx_files:
-        return docx_files[0]
-    else:
-        return None
-
-
 if __name__ == "__main__":
     # Get the first .docx file name in the current directory
-    directory_path = "."
-    docx_filename = get_first_docx_filename(directory_path)
-    if docx_filename is not None:
-        extracted_text = extract_text_from_docx(docx_filename)
-        filename = "output.asn1"
-        with open(filename, "w") as file:
+    # check argv[1] to get the path
+    if len(argv) < 2:
+        print("Usage: python decode_asn1.py <filename>")
+        print("Or: python decode_asn1.py <filename> <outputfile>")
+        exit(1)
+    extracted_text = extract_text_from_docx(argv[1])
+    if len(argv) > 3:
+        filename = argv[2]
+        with open(filename, "w", encoding="utf-8") as file:
             file.write(extracted_text)
-        print(f"Extracted in {filename}")
+    else:
+        print(extracted_text)
